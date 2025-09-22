8f648a468698732f669d07dff594d1e6a4abd5ce
Anushka A. Bhasme <78693700+https-ab@users.noreply.github.com>
2025-09-22 11:37:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let squares = document.querySelectorAll(".square");
+
+squares.forEach(square => {
+	square.addEventListener("mouseover", () => {
+		squares.forEach(s, => {
+			 if (s !== square) {
+		        s.style.backgroundColor = '#6F4E37'; // Coffee color
+		     }
+		});
+	});
+
+	square.addEventListener("mouseout", () => {
+		squares.forEach(s, => {
+			s.style.backgroundColor = "#E6E6FA";
+		})
+	})
+});
